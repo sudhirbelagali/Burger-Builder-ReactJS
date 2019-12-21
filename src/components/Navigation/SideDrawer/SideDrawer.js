bb666526413b29541b9575b5b9b66f30f42ae224
Sudhir Belagali <sudhirbelagalisdm@gmail.com>
2019-12-21 15:24:20 +0530

    Modified the logo in side drawer and toolbar- responsive design

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,13 +1,16 @@
+
 import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css'; 
+import classes from './SideDrawer.css';
 
 const sideDrawer = () => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo height="11%" />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
